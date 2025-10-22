b290ae8306c56a82d20a83f91458eeaab788b457
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-10-22 16:05:17 +0200

    TP0_calculette

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+/**
+ *
+ * @author kassab10
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Please enter the operator :
+        1) add 
+    }
+    
+}
