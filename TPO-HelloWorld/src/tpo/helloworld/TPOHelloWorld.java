5ab0b7563276a7fbb962d024c78a3b7a02d60acf
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-10-22 16:04:32 +0200

    TP0_Helloworld

---
+++ TPO-HelloWorld/src/tpo/helloworld/TPOHelloWorld.java
@@ -0,0 +1,20 @@
+/*TP0 HelloWorld, 20/10/2025 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpo.helloworld;
+
+/**
+ *
+ * @author kassab10
+ */
+public class TPOHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+    }
+
+}
