5a1cb3e52304c8fb830b807bb0c2ccb2d834cd03
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-10-24 10:10:53 +0200

    TP0_calculette

---
+++ TP0_calculette/src/tp0_calculette/Scanner.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp0_calculette;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author kassab10
+ */
+class Scanner {
+
+    Scanner(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String nextLine() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- TP0_calculette/src/tp0_calculette/TP0_calculette.java
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp0_calculette;
-
+    import java.util.Scanner ;
 /**
  *
  * @author kassab10
@@ -14,8 +14,13 @@ public class TP0_calculette {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Please enter the operator :
-        1) add 
+        System.out.println("Please enter the operator:\n 1) add\n  2)substract \n 3) multiply \n 4) divide \n" );
+        int operateur
+        operateur = 0 ;
+        String nombre ;
+        Scanner sc ;
+        sc = new Scanner (System.in);
+        nombre = sc.nextLine ();
     }
     
 }
