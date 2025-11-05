87e2d4db91223f372b9888bbdd2e43ba2646f047
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-11-05 11:03:17 +0100

    TP2_exo3_début

---
+++ TP2_relation_1 _ KASSAB/src/tp2_relation_1/pkg_/kassab/Personne.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1.pkg_.kassab;
+
+/**
+ *
+ * @author kassab10
+ */
+public class Personne {
+    
+}
