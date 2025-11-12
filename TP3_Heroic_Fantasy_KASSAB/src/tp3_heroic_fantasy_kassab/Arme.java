478af4b680ee18e6af9b3b61eebf200796e91683
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-11-12 12:45:30 +0100

    TP3_exo1_début

---
+++ TP3_Heroic_Fantasy_KASSAB/src/tp3_heroic_fantasy_kassab/Arme.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_kassab;
+
+/**
+ *
+ * @author kassab10
+ */
+public abstract class Arme {
+    String nom ; 
+    int niveauAttaque ;  
+    public Arme(int niveauAttaque) {
+        int niveauAttaque <= 100 ; 
+}
+
