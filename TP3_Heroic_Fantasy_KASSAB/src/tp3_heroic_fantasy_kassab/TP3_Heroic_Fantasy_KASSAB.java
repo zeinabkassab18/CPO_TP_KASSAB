3e71f0f80471351e29fb8221c84562c5edf9abbf
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-11-19 11:02:19 +0100

    TP3 en cours

--- TP3_Heroic_Fantasy_KASSAB/src/tp3_heroic_fantasy_kassab/TP3_Heroic_Fantasy_KASSAB.java
+++ TP3_Heroic_Fantasy_KASSAB/src/tp3_heroic_fantasy_kassab/TP3_Heroic_Fantasy_KASSAB.java
@@ -42,11 +42,44 @@ public class TP3_Heroic_Fantasy_KASSAB {
         personnages.add(conan);
         personnages.add(lannister);
 
-        System.out.println("\n=== Personnages ===");
+        System.out.println("\n Personnages");
         for (Personnage p : personnages) {
             System.out.println(p);
         }
+        
+        System.out.println("\n=== Point 3 : Gestion des armes ===");
+
+ 
+        Guerrier g = new Guerrier("Arthur", 80, true);      
+        Magicien m = new Magicien("Merlin", 70, false);     
+
+        Baton b1 = new Baton("Bâton ancien", 3, 2);
+        Baton b2 = new Baton("Bâton elfique", 4, 4);
+        Baton b3 = new Baton("Bâton runique", 5, 6);
+
+        Epee e1 = new Epee("Lame rapide", 6, 3);
+        Epee e2 = new Epee("Épée lourde", 7, 5);
+        Epee e3 = new Epee("Rapière", 8, 4);
+
+        g.ajouterArme(b1);
+        g.ajouterArme(e1);
+        g.ajouterArme(e2);
+        g.equiper("Lame rapide");
+
+      
+        m.ajouterArme(b2);
+        m.ajouterArme(b3);
+        m.ajouterArme(e3);
+
+      
+        System.out.println("\nNombre de bâtons de " + m.getNom() + " : " + m.getNbBatons());
+
+        System.out.println("\n=== Personnages (point 3) ===");
+        System.out.println(g);
+        System.out.println(m);
     }
 }
+  
+
 
    
