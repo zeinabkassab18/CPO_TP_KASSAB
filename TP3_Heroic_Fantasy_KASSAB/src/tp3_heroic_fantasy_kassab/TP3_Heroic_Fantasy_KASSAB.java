b07484a2ed900752aa31061f94be2fec51360120
zeinabkassab18 <kassab.zeinab180105@gmail.com>
2025-11-19 10:09:52 +0100

    TP3 en cours

--- TP3_Heroic_Fantasy_KASSAB/src/tp3_heroic_fantasy_kassab/TP3_Heroic_Fantasy_KASSAB.java
+++ TP3_Heroic_Fantasy_KASSAB/src/tp3_heroic_fantasy_kassab/TP3_Heroic_Fantasy_KASSAB.java
@@ -4,18 +4,49 @@
  */
 package tp3_heroic_fantasy_kassab;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author kassab10
  */
 public class TP3_Heroic_Fantasy_KASSAB {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Arme.nom 
+
+        Epee excalibur = new Epee("Excalibur", 7, 5);
+        Epee durandal = new Epee("Durandal", 4, 7);
+
+        Baton chene = new Baton("Chêne", 4, 5);
+        Baton charme = new Baton("Charme", 5, 6);
+
+        ArrayList<Arme> armes = new ArrayList<>();
+
+        armes.add(excalibur);
+        armes.add(durandal);
+        armes.add(chene);
+        armes.add(charme);
+
+        for (int i = 0; i < armes.size(); i++) {
+            System.out.println(armes.get(i));
+        }
+        Magicien gandalf   = new Magicien("Gandalf", 65, true);   // confirmé
+        Magicien garcimore = new Magicien("Garcimore", 44, false); // novice
+
+        Guerrier conan     = new Guerrier("Conan", 78, false);    // à pied
+        Guerrier lannister = new Guerrier("Lannister", 45, true); // à cheval
+
+        ArrayList<Personnage> personnages = new ArrayList<>();
+        personnages.add(gandalf);
+        personnages.add(garcimore);
+        personnages.add(conan);
+        personnages.add(lannister);
+
+        System.out.println("\n=== Personnages ===");
+        for (Personnage p : personnages) {
+            System.out.println(p);
+        }
     }
-    
 }
+
+   
